6f69dc1096d7212089b67d64c7fa800a5f8e4df5
Basler182 <showdialog@web.de>
2020-03-21 15:16:08 +0100

    splash page

---
+++ app/src/main/java/com/ks/einanrufhilft/view/splash/splash.java
@@ -0,0 +1,39 @@
+package com.ks.einanrufhilft.view.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.ks.einanrufhilft.R;
+import com.ks.einanrufhilft.view.login.LoginActivity;
+
+public class splash extends AppCompatActivity {
+
+    private Handler myHandler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        myHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startLogin();
+            }
+        }, 900);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void startLogin() {
+        this.startActivity(new Intent(this, LoginActivity.class));
+    }
+}
