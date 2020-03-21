0e69d8b43e82eb4f8b9cf85d22a8c586ca963446
Felix Beil <webmaster@moonlight.wtf>
2020-03-21 22:20:06 +0100

    Add Google Maps fragment
    Replace the map view with the SupportMapFragment (makes it easier to
    use). Add Google Maps API key to be able to use the map.

--- app/src/main/java/com/ks/einanrufhilft/view/home/Home.java
+++ app/src/main/java/com/ks/einanrufhilft/view/home/Home.java
@@ -1,35 +1,50 @@
 package com.ks.einanrufhilft.view.home;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.ks.einanrufhilft.R;
 import com.ks.einanrufhilft.persistance.OrderDTO;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements OnMapReadyCallback {
 
     private List<OrderDTO> orders;
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        initalizeData();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
 
+        initalizeData();
     }
 
-
-    private void initalizeData(){
+    private void initalizeData() {
         orders = new ArrayList<>();
         orders.add(new OrderDTO());
         orders.add(new OrderDTO());
         orders.add(new OrderDTO());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51, 10), 5));
     }
 }
