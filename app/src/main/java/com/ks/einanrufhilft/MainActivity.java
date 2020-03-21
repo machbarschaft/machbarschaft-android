ffb2b9e4d088c60be6f5c145a97968dfe5bad38f
Basler182 <showdialog@web.de>
2020-03-21 14:07:23 +0100

    removedfirebase messaging

--- app/src/main/java/com/ks/einanrufhilft/MainActivity.java
+++ app/src/main/java/com/ks/einanrufhilft/MainActivity.java
@@ -2,7 +2,9 @@ package com.ks.einanrufhilft;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.ks.einanrufhilft.R;
 
 public class MainActivity extends AppCompatActivity {
