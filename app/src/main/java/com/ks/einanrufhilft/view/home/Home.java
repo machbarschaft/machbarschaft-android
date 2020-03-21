2a1503b2d03f4b5fbb77ee0f20b01532ba6ca74a
Basler182 <showdialog@web.de>
2020-03-21 21:45:27 +0100

    added adapter for recycler view

--- app/src/main/java/com/ks/einanrufhilft/view/home/Home.java
+++ app/src/main/java/com/ks/einanrufhilft/view/home/Home.java
@@ -1,13 +1,15 @@
 package com.ks.einanrufhilft.view.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
 
+import com.ks.einanrufhilft.Database.Database;
+import com.ks.einanrufhilft.Database.Entitie.Account;
 import com.ks.einanrufhilft.R;
-import com.ks.einanrufhilft.persistance.OrderDTO;
+import com.ks.einanrufhilft.Database.OrderDTO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,13 +17,33 @@ import java.util.List;
 public class Home extends AppCompatActivity {
 
     private List<OrderDTO> orders;
+    private RecyclerView recyclerView;
+    private OrderAdapter orderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
 
+        /* TESTS */
+        Database db = Database.getInstance();
+        // String phone_number, String plz, String strasse, String hausnummer, String firstName, String lastNamme, String[] category) {
+        //        this.phone_number = phone_number
+        // Order o = new Order("0981238231", "12212", "myStarsse", "12a","alex",
+        //       "maier", new String[]{"Einkauf"});
+
+        Account a = new Account("Max", "maier", "90821389123",7.5f, 30);
+
+        db.getOrders();
+        db.setOrderConfirmed("mofVj419q6fAxj4hLYeW", Database.Status.Confirmed);
+
+        //db.createAccount(a);
+
+        /* TESTS */
+
         initalizeData();
+        initView();
 
     }
 
@@ -31,5 +53,12 @@ public class Home extends AppCompatActivity {
         orders.add(new OrderDTO());
         orders.add(new OrderDTO());
         orders.add(new OrderDTO());
+    }
+
+    public void initView(){
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        orderAdapter = new OrderAdapter(this, (ArrayList<OrderDTO>) orders);
+        recyclerView.setAdapter(orderAdapter);
     }
 }
---
+++ app/src/main/java/com/ks/einanrufhilft/view/home/OrderAdapter.java
@@ -0,0 +1,63 @@
+package com.ks.einanrufhilft.view.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ks.einanrufhilft.R;
+import com.ks.einanrufhilft.Database.OrderDTO;
+
+import java.util.ArrayList;
+
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderHolder> {
+
+    private Context context;
+    private ArrayList<OrderDTO> orders;
+
+    public OrderAdapter(Context context, ArrayList<OrderDTO> orders) {
+        this.context = context;
+        this.orders = orders;
+    }
+
+    @NonNull
+    @Override
+    public OrderHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recyclerview_item, parent, false);
+        return new OrderHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderHolder holder, int position) {
+        OrderDTO order = orders.get(position);
+        holder.setDetails(order);
+    }
+
+    @Override
+    public int getItemCount() {
+        return orders.size();
+    }
+
+    class OrderHolder extends RecyclerView.ViewHolder {
+
+        private TextView orderType, einkaufsliste, distance;
+
+        OrderHolder(View itemView) {
+            super(itemView);
+            orderType = itemView.findViewById(R.id.textViewTitle);
+            einkaufsliste = itemView.findViewById(R.id.textViewShortDesc);
+            distance = itemView.findViewById(R.id.textViewDistance);
+        }
+
+        void setDetails(OrderDTO order) {
+            orderType.setText(order.getCategory());
+            einkaufsliste.setText(order.getEinkaufszettel());
+            distance.setText("200 Meter entfernt..");
+            //txtDiameter.setText("test");
+        }
+    }
+}
