cba23441d1904f353c93787f5476c3717c4490bf
Basler182 <showdialog@web.de>
2020-03-22 11:52:30 +0100

    safe order in progress in sharedpref

--- app/src/main/java/com/ks/einanrufhilft/Database/Storage.java
+++ app/src/main/java/com/ks/einanrufhilft/Database/Storage.java
@@ -1,15 +1,24 @@
 package com.ks.einanrufhilft.Database;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.widget.EditText;
+
+import com.ks.einanrufhilft.util.ApplicationConstants;
+
 public class Storage {
     private static Storage storage;
 
     private String userId;
     private String currentOrderId;
+
     private Storage() {
 
     }
+
     public static Storage getInstance() {
-        if(storage == null) {
+        if (storage == null) {
             storage = new Storage();
         }
         return storage;
@@ -29,5 +38,18 @@ public class Storage {
 
     public void setCurrentOrderId(String currentOrderId) {
         this.currentOrderId = currentOrderId;
+    }
+
+
+    public boolean setOrderIdInProgress(Context context, String currentOrderId) {
+        SharedPreferences pref = context.getSharedPreferences(ApplicationConstants.SHARED_PREF_ORDERINPROGRESS, 0);
+        Editor editor = pref.edit();
+        editor.putString(ApplicationConstants.SHARED_PREF_ORDERINPRGRESS_KEY, currentOrderId);
+        return editor.commit();
+    }
+
+    public String getOrderIdInPRogress(Context context) {
+        SharedPreferences pref = context.getSharedPreferences(ApplicationConstants.SHARED_PREF_ORDERINPROGRESS, 0);
+        return pref.getString(ApplicationConstants.SHARED_PREF_ORDERINPRGRESS_KEY, null);
     }
 }
