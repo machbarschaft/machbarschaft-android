ee3ec8a38c68f569d3de4d1dc2851f56b4f7d1f3
Julian Wieners <julianwieners@Julians-MBP.fritz.box>
2020-03-22 17:57:29 +0100

    added phone number verification

--- app/src/main/java/com/ks/einanrufhilft/view/register/RegisterActivity.java
+++ app/src/main/java/com/ks/einanrufhilft/view/register/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -17,6 +18,8 @@ import com.ks.einanrufhilft.view.login.*;
 
 import com.passbase.passbase_sdk.Passbase;
 import com.passbase.passbase_sdk.PassbaseButton;
+
+import java.util.Random;
 
 import kotlin.Pair;
 import kotlin.Unit;
@@ -111,11 +114,12 @@ public class RegisterActivity extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(trusted) {
+                if(true) {
                     if(agbAcepted) {
-                        // TODO Send code for phone verification
-                        // Transfer user data in array: name, surname, address, phone number
-                        String[] data = {tfName.getText().toString(), tfSurname.getText().toString(), tfAddress.getText().toString(), tfPhone.getText().toString()};
+                        String code = getCode();
+                        sendSMS(tfPhone.getText().toString(), code);
+                        // Transfer user data in array: phone code, name, surname, address, phone number
+                        String[] data = {code, tfName.getText().toString(), tfSurname.getText().toString(), tfAddress.getText().toString(), tfPhone.getText().toString()};
                         Intent i = new Intent(context, VerifyPhoneActivity.class);
                         i.putExtra("registerData", data);
                         startActivity(i);
@@ -129,5 +133,30 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void sendSMS(String phoneNo, String code) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, code, null, null);
+            Toast.makeText(getApplicationContext(), "Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage().toString(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+    private String getCode() {
+        Random r = new Random();
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        String result = "";
+        for(int i=0; i<4; i++) {
+            result = result + array[r.nextInt(9)];
+        }
+
+        return result;
     }
 }
