38609f32fbaf46ab0b7d7863f7f104613b2757cc
Basler182 <showdialog@web.de>
2020-03-26 13:26:14 +0100

    -splashscreen to fullscreen

--- app/src/main/java/com/ks/einanrufhilft/view/splash/Splash.java
+++ app/src/main/java/com/ks/einanrufhilft/view/splash/Splash.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import com.ks.einanrufhilft.Database.Database;
 import com.ks.einanrufhilft.R;
@@ -23,6 +24,7 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
 
