d137ce60dbdd404d544e56ad02ea3a7bfb17e7c5
Julian Wieners <julianwieners@Julians-MBP.fritz.box>
2020-03-28 22:33:30 +0100

    Fixed compilation errors

--- app/src/main/java/jetzt/machbarschaft/android/view/login/LoginMain.java
+++ app/src/main/java/jetzt/machbarschaft/android/view/login/LoginMain.java
@@ -55,7 +55,6 @@ public class LoginMain extends AppCompatActivity {
 
         phoneNumber = findViewById(R.id.input_phone_number);
         loginButton = findViewById(R.id.btn_login);
-        Button registerButton = findViewById(R.id.btn_register);
 
 
         /* TODO uncomment if deploy
@@ -65,14 +64,9 @@ public class LoginMain extends AppCompatActivity {
         */
 
         loginButton.setOnClickListener(v -> login());
-
-        registerButton.setOnClickListener(v -> {
-            Intent i = new Intent(context, RegisterActivity.class);
-            startActivity(i);
-        });
     }
 
-
+    // TODO Check if phone number is registered
     public void login() {
         if (validate()) {
             loginButton.setEnabled(false);
