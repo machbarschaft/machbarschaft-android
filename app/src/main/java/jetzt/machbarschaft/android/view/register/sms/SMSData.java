ec41366245bec924aaed396c4220525d0d62d06f
Jesse Palarus <jtsharkjtshark@gmail.com>
2020-03-31 12:48:24 +0200

    Add callbacks to the SMSData

--- app/src/main/java/jetzt/machbarschaft/android/view/register/sms/SMSData.java
+++ app/src/main/java/jetzt/machbarschaft/android/view/register/sms/SMSData.java
@@ -2,8 +2,11 @@ package jetzt.machbarschaft.android.view.register.sms;
 
 import java.io.Serializable;
 
-import jetzt.machbarschaft.android.database.entitie.Account;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Data
 public class SMSData implements Serializable
 {
     /**
@@ -11,29 +14,5 @@ public class SMSData implements Serializable
      */
     private String verificationId;
 
-    private final Account account;
-
-    public SMSData(Account account) {
-        this.account = account;
-    }
-
-    public String getVerificationId() {
-        return verificationId;
-    }
-
-    public void setVerificationId(String verificationId) {
-        this.verificationId = verificationId;
-    }
-
-    public Account getAccount()
-    {
-        return account;
-    }
-
-    /**
-     * The Phone number from the user
-     */
-    public String getPhoneNumber() {
-        return account.getPhone_number();
-    }
+    private final String phoneNumber;
 }
