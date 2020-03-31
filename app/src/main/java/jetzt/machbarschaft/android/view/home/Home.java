9457108b1b616ef4b333e87b1edeb2df90fa35db
Basler182 <showdialog@web.de>
2020-03-31 16:17:46 +0200

    - redesigned order_accept (step 1)

--- app/src/main/java/jetzt/machbarschaft/android/view/home/Home.java
+++ app/src/main/java/jetzt/machbarschaft/android/view/home/Home.java
@@ -11,6 +11,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
