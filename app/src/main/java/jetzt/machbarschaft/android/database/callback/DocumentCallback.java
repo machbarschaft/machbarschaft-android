ffc513b68acb6737bbe12d55c1e72402d8da0cf0
Bene <bene_b.99@web.de>
2020-04-02 14:23:44 +0200

    started implementing authentication

--- app/src/main/java/jetzt/machbarschaft/android/database/callback/DocumentCallback.java
+++ app/src/main/java/jetzt/machbarschaft/android/database/callback/DocumentCallback.java
@@ -2,8 +2,6 @@ package jetzt.machbarschaft.android.database.callback;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 
-import java.io.UnsupportedEncodingException;
-
 public interface DocumentCallback {
     void onDocumentLoad(DocumentSnapshot document);
 }
