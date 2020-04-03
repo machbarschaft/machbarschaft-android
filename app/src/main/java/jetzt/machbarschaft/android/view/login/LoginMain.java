8af98829c825c25173500661f3c51b229c3d50d1
Basler182 <showdialog@web.de>
2020-04-03 15:21:26 +0200

    - added functionality to get the Mobile Number in a uniform way on the Login Screen

--- app/src/main/java/jetzt/machbarschaft/android/view/login/LoginMain.java
+++ app/src/main/java/jetzt/machbarschaft/android/view/login/LoginMain.java
@@ -8,9 +8,15 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -38,6 +44,8 @@ public class LoginMain extends AppCompatActivity {
     private Button loginButton;
     private Context context;
     private ProgressDialog progressDialog;
+    private AutoCompleteTextView countryCodeTextView;
+
     @Override
     public void onResume() {
         super.onResume();
@@ -67,17 +75,23 @@ public class LoginMain extends AppCompatActivity {
 
         // Button click handlers
         loginButton.setOnClickListener(v -> login());
+
+        String[] countryCodes = getResources().getStringArray(R.array.countryCode_spinner_array);
+        ArrayAdapter<String> countryCodeAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_menu_popup_item, countryCodes);
+        countryCodeTextView =
+                findViewById(android.R.id.content)
+                        .findViewById(R.id.filled_exposed_dropdown);
+        countryCodeTextView.setText(countryCodes[0], false);
+        countryCodeTextView.setAdapter(countryCodeAdapter);
     }
 
     public void login() {
         if (validate()) {
             loginButton.setEnabled(false);
             progressDialog.show();
-            String phoneNumberStr = phoneNumber.getText().toString();
-
-            DataAccess.getInstance().existUser(phoneNumberStr,exist -> {
+            DataAccess.getInstance().existUser(getPhoneNumber(), exist -> {
                 if (exist) {
-                    SMSManager.getInstance().sendSMS(new SMSData(phoneNumberStr), this, new SMSEventListenerImpl() {
+                    SMSManager.getInstance().sendSMS(new SMSData(getPhoneNumber()), this, new SMSEventListenerImpl() {
                         @Override
                         public void onNumberWrongFormatted(Exception firebaseException, Activity activity) {
                             progressDialog.dismiss();
@@ -91,14 +105,11 @@ public class LoginMain extends AppCompatActivity {
                             onLoginFailed();
                         }
                     });
-                }
-                else
-                {
+                } else {
                     Toast.makeText(getApplicationContext(), R.string.login_error_number_not_exist, Toast.LENGTH_SHORT).show();
                     onErrorWhileLoading();
                 }
             });
-
 
 
         } else {
@@ -126,8 +137,7 @@ public class LoginMain extends AppCompatActivity {
         return valid;
     }
 
-    private void onErrorWhileLoading()
-    {
+    private void onErrorWhileLoading() {
         loginButton.setEnabled(true);
         progressDialog.dismiss();
     }
@@ -138,5 +148,20 @@ public class LoginMain extends AppCompatActivity {
      */
     private void onLoginFailed() {
         Toast.makeText(getApplicationContext(), R.string.login_error_generic, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Adds the Country Code to the Phone Number and removes unnecessary Zeros in Front of the Number,
+     * in case the user doesn't know, that there should not follow a Zero directly after Country Code.
+     * Also removes Whitespaces with Regex inside of the number.
+     * Accepts Phone Numbers in Style of: 0176 11111111, 176 11111111, 17611111111, 176 1111 1111
+     * So Phone Numbers will uniform like: <br>
+     * +49176222222
+     *
+     * @return phone Number
+     */
+    public String getPhoneNumber() {
+        String secondPartNumber = phoneNumber.getText().toString();
+        return countryCodeTextView.getText().toString() + secondPartNumber.replaceAll("^0+", "").replaceAll("\\s+", "");
     }
 }
