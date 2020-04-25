e736dbcb5d7d08e6011d2157ec0fe37f9accbe18
Basler182 <showdialog@web.de>
2020-04-25 15:38:50 +0200

    - fix Google Maps Bug

--- app/src/main/java/jetzt/machbarschaft/android/view/splash/Splash.java
+++ app/src/main/java/jetzt/machbarschaft/android/view/splash/Splash.java
@@ -1,6 +1,8 @@
 package jetzt.machbarschaft.android.view.splash;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -8,6 +10,8 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.File;
 
 import jetzt.machbarschaft.android.R;
 import jetzt.machbarschaft.android.database.Authentication;
@@ -27,7 +31,7 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-
+        fixGoogleMapBug();
         FirebaseUser user = Authentication.getInstance().getCurrentUser();
         if (user == null) {
             myHandler.postDelayed(this::startLogin, 1200);
@@ -38,6 +42,15 @@ public class Splash extends AppCompatActivity {
         // TESTS
         // DataAccessTest.getInstance().runTests(this);
         //TESTS
+    }
+
+    private void fixGoogleMapBug() {
+        SharedPreferences googleBug = getSharedPreferences("google_bug", Context.MODE_PRIVATE);
+        if (!googleBug.contains("fixed")) {
+            File corruptedZoomTables = new File(getFilesDir(), "ZoomTables.data");
+            corruptedZoomTables.delete();
+            googleBug.edit().putBoolean("fixed", true).apply();
+        }
     }
 
     @Override
