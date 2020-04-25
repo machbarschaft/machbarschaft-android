06917b11e196a3529a5693e12880376f639fdc40
Felix Beil <webmaster@moonlight.wtf>
2020-04-25 13:18:48 +0200

    Remove Passbase sdk
    Remove the dependency for the Passbase SDK and its maven repository.
    Remove the Passbase related code and layout.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 Project Machbarschaft was created in the context of [WirVsVirus Hackathon](https://wirvsvirushackathon.org/) hosted by the German Government. Our pitch video can be found [on youtube](https://www.youtube.com/watch?v=8YJ0I0dMmWg). We also have a [Devpost Profile](https://devpost.com/software/einanrufhilft) and a website [machbarschaft.jetzt](https://machbarschaft.jetzt/).
 
-This repository contains the Android Application which is meant for the helper, to find nearby help seeking persons. New Users have to verify their Identiy with Passbase(ID - Verfication) and SMS. Afterwards they can help others in need. 
+This repository contains the Android Application which is meant for the helper, to find nearby help seeking persons. New Users have to verify their Identiy with their phone number via SMS. Afterwards they can help others in need.
 ## Demo
 
 We have recorded a short video to demonstrate the current state of the app: [Youtube-Video](https://www.youtube.com/watch?v=Lg5ecVKiLGc)
@@ -18,7 +18,7 @@ We have recorded a short video to demonstrate the current state of the app: [You
 ## How it works
 
 1. Install the MACHBARSCHAFT app to your Android device.
-2. Click "Register" and fill the form with your personal data. Then verify your identity via Passbase.
+2. Click "Register" and enter yout phone number.
 3. You will get a SMS with a code from us. Type in this code on the shown page.
 4. You are almost done! Your account was created successful. Now you can login with your phone number.
 5. Search a job in your location. Then you call the person seeking help and make your way.
@@ -40,7 +40,7 @@ We have recorded a short video to demonstrate the current state of the app: [You
 #### Steps:
 * Implement pages from wireframe
 * Implement button interaction
-* Implement registration and verification via passbase
+* ~Implement registration and verification via passbase~
 * Implement placeholder orders and google maps API
 * Implement Database interactions 
 * Add designs to app
--- app/src/main/java/jetzt/machbarschaft/android/view/register/RegisterActivity.java
+++ app/src/main/java/jetzt/machbarschaft/android/view/register/RegisterActivity.java
@@ -9,22 +9,16 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.passbase.passbase_sdk.Passbase;
-import com.passbase.passbase_sdk.PassbaseButton;
-
 import jetzt.machbarschaft.android.R;
-import jetzt.machbarschaft.android.util.PhoneNumberFormatterUtil;
 import jetzt.machbarschaft.android.view.login.LoginMain;
-import kotlin.Pair;
 
 /**
- * Handles the Events to register. Includes the Passbase Video Ident and the registering with mobile number.
+ * Handles the Events to register. Includes the registration with mobile number.
  */
 public class RegisterActivity extends AppCompatActivity {
     private boolean trusted = false;
@@ -60,40 +54,8 @@ public class RegisterActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(v -> startActivity(new Intent(getApplicationContext(), LoginMain.class)));
         toolbar.getNavigationIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
 
-        // Add Passbase verification button
-        // User need to verify his identity via a official document
-        final Passbase passbaseRef = new Passbase(this);
-
-        passbaseRef.initialize(
-                "3e27309be36f707c9fea64ef81f22d011ed52942952b9e96cb5e5eff7db2c13e",
-                "",
-                new Pair[]{}
-        );
-
-        PassbaseButton verificationButton = findViewById(R.id.passbase_verification_button);
-
-        // Passbase button click handler
-        verificationButton.setOnClickListener(view -> passbaseRef.startVerification());
-
-        // Callbacks for verification
-        passbaseRef.onCancelPassbaseVerification(() -> {
-            Toast.makeText(this, R.string.verify_error_generic, Toast.LENGTH_LONG).show();
-            trusted = false;
-
-            return null;
-        });
-
-        passbaseRef.onCompletePassbaseVerification(authKey -> {
-            Toast.makeText(this, R.string.verify_success, Toast.LENGTH_LONG).show();
-            trusted = true;
-
-            return null;
-        });
-
         // Button click handlers
         agbBox.setOnClickListener(view -> agbAccepted = ((CheckBox) view).isChecked());
-
-        // TODO Get user information for new account from passbase key
 /*
         btnSend.setOnClickListener(view -> {
             if (tfName.getText().toString().isEmpty() || tfSurname.getText().toString().isEmpty() || tfAddress.getText().toString().isEmpty() || PhoneNumberFormatterUtil.getPhoneNumber(countryCodeTextView.getText().toString(), phoneSecondPart.getText().toString())) {
