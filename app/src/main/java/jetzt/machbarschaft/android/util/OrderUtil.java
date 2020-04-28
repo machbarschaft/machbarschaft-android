56daf3b34df7d2d8dcdaf0dce23b3c418eead4eb
Basler182 <showdialog@web.de>
2020-04-28 21:36:01 +0200

    OrderUtil with Order Cancel procedure

---
+++ app/src/main/java/jetzt/machbarschaft/android/util/OrderUtil.java
@@ -0,0 +1,30 @@
+package jetzt.machbarschaft.android.util;
+
+import android.content.Context;
+import android.content.Intent;
+
+
+import jetzt.machbarschaft.android.database.DataAccess;
+import jetzt.machbarschaft.android.database.Storage;
+import jetzt.machbarschaft.android.database.entitie.Order;
+import jetzt.machbarschaft.android.database.entitie.OrderSteps;
+import jetzt.machbarschaft.android.services.OrderInProgressNotification;
+import jetzt.machbarschaft.android.view.home.Home;
+
+public class OrderUtil {
+
+    /**
+     *  Cancels the current order.
+     *  It will be opened again the Database and displayed to other helpers.
+     *  Also the notification will disappear and the current steps will be reset in the storage.
+     */
+
+    public static void cancelOrder(Order mOrder, Context mContext) {
+            DataAccess.getInstance().setOrderStatus(mOrder.getId(), Order.Status.OPEN);
+            Storage.getInstance().setCurrentStep(mContext, OrderSteps.STEP0_NONE);
+            Storage.getInstance().setActiveOrder(mContext, false);
+            Intent serviceIntent = new Intent(mContext, OrderInProgressNotification.class);
+            mContext.stopService(serviceIntent); //stops the foregroundservice
+            mContext.startActivity(new Intent(mContext, Home.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+}
